c591b4dfe0833e8088a6248b953940aa974427cc
Shuyi Hoo <anloveshu@Shuyis-MacBook-Air.local>
2023-01-25 19:21:46 -0800

    added pos boolean to set black and red

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let flag = true; //set to true
-for(let i = 0; i < 550; i+=50){
-    for(let j = 0; j < 550; j+=50){
-        let elem = document.createElement("div");
-        if(flag == true){
-            color = "black";
-        }else{
-            color ="red";
+let pos = true; //set to true
+for(let i = 0; i < 550; i+=50){ //for loop to create row i
+    for(let j = 0; j < 550; j+=50){ //nested for loop to create row j
+        let elem = document.createElement("div"); //set elem to createElement function
+        if(pos == true){ //if pos is true
+            color = "black"; //set color to black
+        }else{ //if pos is not true
+            color ="red"; //set color to red
         }
-        flag = !flag;
+        pos = !pos; //set pos to false outside the loop
         let style_string = "border:0px solid black;position:absolute;background-color:" + color + ";height:50px;width:50px;top:" + i + "px;left:" + j + "px";
         elem.setAttribute("style", style_string);
         document.body.append(elem);
@@ -16,16 +16,3 @@ for(let i = 0; i < 550; i+=50){
 
 //elem.setAttribute("style", "position:absolute;top:150px;left:150px;background-color:red;height:50px;width:50px");
 //document.body.append(elem);
-
-
-/*var chessboard = document.getElementById('chessboard');
-   for (var i = 0; i < 8; i++) {
-      for (var j = 0; j < 8; j++) {
-         var chessSquare = document.createElement('div');
-         chessSquare.className = 'chess-square';
-         if ((i + j) % 2 == 0) {
-            chessSquare.style.backgroundColor = '#000';
-         }
-         chessboard.appendChild(chessSquare);
-      }
-   }*/
